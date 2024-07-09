457cd73b5a02a8bbf4e8ef6b0f77f0081fabcdf7
Thunyanan TANGPIPATPONG <87270595+wannaeattaco@users.noreply.github.com>
2024-07-09 14:30:11 +0700

    finished project

---
+++ src/sections/Contact/Contact.jsx
@@ -0,0 +1,46 @@
+import styles from './ContactStyles.module.css'
+function Contact() {
+  return (
+    <section id='contact' className={styles.container}>
+      <h1 className='sectionTitle'>Contact</h1>
+      <form action=''>
+        <div>
+            <label htmlFor='name' hidden>
+                Name
+            </label>
+            <input
+            type='text'
+            name="name"
+            id="name"
+            placeholder='Name'
+            required/>
+        </div>
+        <div>
+            <label htmlFor='email' hidden>
+                Email
+            </label>
+            <input
+            type='text'
+            name="email"
+            id="email"
+            placeholder='Email'
+            required/>
+        </div>
+        <div>
+            <label htmlFor='message' hidden>
+                Message
+            </label>
+            <textarea
+            type='text'
+            name="message"
+            id="message"
+            placeholder='Message'
+            required></textarea>
+        </div>
+        <input className='"hover btn' type='submit' value='Submit'/>
+      </form>
+    </section>
+  )
+}
+
+export default Contact
---
+++ src/sections/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import style from './FooterStyles.module.css'
+function Footer() {
+    return (
+     <section id='footer' className={style.container}>
+        <p>&copy; 2024 Thunyanan Tangpipatpong.<br/>
+        All rights reserved</p>
+     </section>
+    )
+  }
+
+export default Footer
